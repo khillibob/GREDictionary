c2bc21a9c81d035e1e9e1efda8c4feef5a64030f
khillibob <khillibob@gmail.com>
2016-09-04 20:21:55 +0530

    mnemonic parser updated

--- application/Utils/dbUtil/dbUtils.py
+++ application/Utils/dbUtil/dbUtils.py
@@ -3,6 +3,8 @@ import os
 from pprint import pprint
 import json
 import sys
+from shutil import copyfile
+
 
 a= "মেজাজ আচরণ ভাষা প্রভৃতির রুক্ষতা"
 
@@ -28,19 +30,23 @@ class DBUtil():
         print( "Opened database successfully")
         conn.execute('''CREATE TABLE  IF NOT EXISTS MNEMONIC_DICT
                    (word CHAR(100) PRIMARY KEY     NOT NULL,
-                    way_to_remember CHAR(500));''')
+                    way_to_remember CHAR(500),
+                    way_to_remember2 CHAR(500));''')
         print("DB created")
         return conn                
-    def insertMnemonic(self,conn,word,way_to_remember):
-        query = "INSERT INTO MNEMONIC_DICT ("+"word"+","+"way_to_remember"+") VALUES ('" +word+"','"+way_to_remember+"')"
+    def insertMnemonic(self,conn,word,way_to_remember,way_to_remember2):
+        query = "INSERT INTO MNEMONIC_DICT ("+"word"+","+"way_to_remember"+","+"way_to_remember2"+") VALUES ('" +word+"','"+way_to_remember+"','"+way_to_remember2+"')"
         print(query)
         conn.execute(query)
         conn.commit()
     
-    def updateMnemonic(self,conn,word,way_to_remember):
+    def updateMnemonic(self,conn=None,word,way_to_remember,way_to_remember2,dbName):
+        conn=sqlite3.connect(dbName)
         updateQuery = "UPDATE MNEMONIC_DICT set "+"way_to_remember = '"+way_to_remember+ "' where word='"+word+"'"
+        updateQuery2 = "UPDATE MNEMONIC_DICT set "+"way_to_remember22 = '"+way_to_remember+ "' where word='"+word+"'"
         print(updateQuery)
         conn.execute(updateQuery)
+        conn.execute(updateQuery2)
         conn.commit()
     
         
@@ -56,31 +62,35 @@ class DBUtil():
                 #print(len(wordList))
                 for word, value in wordList.items():
                     print(word)
-                    self.insertMnemonic(conn,word,'None')
+                    self.insertMnemonic(conn,word,'None','None')
                 #pprint(data)
         """
         if(tableName == 'mnemonic'):
             self.insertMnemonic(conn, word, way_to_remember)
         """    
     
-    def getAllTablenames(self):
+    def getAllTablenames(self,dbName):
         """
         cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
         """
         
-        con = sqlite3.connect('C:\\HDD\\Subhendu\\ProjectX\\GREX\\GRE.db')
+        #con = sqlite3.connect('C:\\HDD\\Subhendu\\ProjectX\\GREX\\GRE.db')
+        con = sqlite3.connect(dbName)
+        
         cursor = con.cursor()
         cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
         #print(cursor.fetchall())
     
-    def getAllWords(self,table):
+    def getAllWords(self,table,dbName):
+        
         """
         def:    fetch all the words from table
         input:    table name
         output:    List of words
         
         """
-        conn = sqlite3.connect('C:\\HDD\\Subhendu\\ProjectX\\GREX\\GRE.db')
+        
+        conn = sqlite3.connect(dbName)
         query = "SELECT word from "+table
         print(query)
         cursor = conn.execute(query)
@@ -91,3 +101,7 @@ class DBUtil():
         print("Operation done successfully")
         return output
     
+    def moveDB(self,src,dst = "C:\\HDD\\Subhendu\\ProjectX\\GREX\\"):
+        if not os.path.exists(dst):
+            os.makedirs(dst)
+        copyfile(src, dst)
--- application/dictionaries/mnemonicDictionary/mnemonicParser.py
+++ application/dictionaries/mnemonicDictionary/mnemonicParser.py
@@ -4,6 +4,12 @@ import application.Utils.UIUtils.seleniumutils as seleniumUtils
 import application.config as config
 import os
 import application.Utils.dbUtil.dbUtils as dbUtils
+import traceback
+
+import urllib
+from bs4 import BeautifulSoup
+
+
 class MnemonicParser():
     
     
@@ -25,20 +31,23 @@ class MnemonicParser():
     def fetchAllWords(self):
         
         """
+        
         def:    get all words from table
         input:    None
         output:    returns all words list
+        
         """
         
         table_name = 'MNEMONIC_DICT'
-        tables = dbUtils.DBUtil().getAllTablenames()
+        dbName = 'C:\\HDD\\Subhendu\\ProjectX\\GREX\\GRE.db'
+        tables = dbUtils.DBUtil().getAllTablenames(dbName)
         print(tables)
-        words = dbUtils.DBUtil().getAllWords(table_name)
+        words = dbUtils.DBUtil().getAllWords(table_name,dbName)
         print(len(words))
         return words
     
     
-    def getEasyMeanings(self,word,webdriver):
+    def getEasyMeanings(self,word):
         """
         definition    :    This method will extract the easy remember technique from Mnemonic 
         input         :    input will be the word and the webdriver
@@ -46,9 +55,39 @@ class MnemonicParser():
                             Structure:
                             result['word'] = ['meaning1','meaning2']
         """
-        SECTION_CSS_SELECTOR = "#home-middle-content > div:nth-child(5)"
-        #a = seleniumUtils.SeleniumUtils(BASE_URL+alphbt)
-        BASE_URL = "http://www.mnemonicdictionary.com/?word="
-         
-         
-MnemonicParser().getEasyMeanings('a',None)            
+        
+        try:
+            SECTION_CSS_SELECTOR = "#home-middle-content div:nth-child(5)"
+            #a = seleniumUtils.SeleniumUtils(BASE_URL+alphbt)
+            BASE_URL = "http://www.mnemonicdictionary.com/?word="
+            page = urllib.request.urlopen(BASE_URL + word).read()
+            soup = BeautifulSoup(page, "html.parser")
+            #page = urllib2.urlopen('http://yahoo.com').read()
+            #soup = BeautifulSoup(page)
+            #sub_page = str(soup.select(SECTION_CSS_SELECTOR))
+            soup = BeautifulSoup(page, "html.parser")
+            divs = soup.find_all("div", attrs={"class": "span9"})
+            print(len(divs))
+            print(divs[0].get_text().strip())
+            if(len(divs)>=2):
+                a='a'
+        except Exception:
+            traceback.print_exc()
+    
+    
+    
+    def insertMnemonic(self):
+        
+        
+        dbname = 'C:\\HDD\\Subhendu\\ProjectX\\GREX\\GRE.db'
+        words = MnemonicParser().fetchAllWords(dbname)
+        for word in words:
+            meanings = self.getEasyMeanings(word)
+            if len(meanings)>=2:
+                dbUtils.DBUtil().updateMnemonic(word, meanings[0], meanings[1],dbname)
+            elif len(meanings)==1:
+                dbUtils.DBUtil().updateMnemonic(word, meanings[0], 'None',dbname)
+            else:
+                dbUtils.DBUtil().updateMnemonic(word, 'None', 'None',dbname)
+MnemonicParser().insertMnemonic()
+#MnemonicParser().getEasyMeanings('pedestrian')            
